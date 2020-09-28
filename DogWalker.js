13ee5f030dabd4c4fbb84a79dee6eaa7bfda4728
Natasha Sana <nfsana@gmail.com>
2020-09-28 16:31:26 -0700

    push to git

--- DogWalker.js
+++ DogWalker.js
@@ -1,9 +1,21 @@
 class DogWalker {
-    numberOfWalksCalculator(dogSizes){
-        let numberOfWalks = -1;
-
-        return numberOfWalks;
+  numberOfWalksCalculator(dogSizes) {
+    // Not sure why numberOfWalks was instantiated to -1 instead of 0;
+    // Will check in with instuctors...
+    let numberOfWalks = 0;
+    let dogList = {};
+    for (let size of dogSizes) {
+      dogList[size] = dogList[size] + 1 || 1;
     }
+    console.log(dogList);
+
+    for (let category in dogList) {
+      numberOfWalks = numberOfWalks + Math.ceil(dogList[category] / 2);
+      //   console.log(numberOfWalks);
+    }
+
+    return numberOfWalks;
+  }
 }
 
 module.exports = DogWalker;
